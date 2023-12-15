569101c54b0ae736b2466bb005a716980723525d
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2023-12-15 11:39:09 +0800

    :heavy_plus_sign: add vue-chart.js

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -5,6 +5,7 @@ import { RouterView } from 'vue-router'
 
 import CDivider from '@/components/CDivider.vue'
 import CEntryList from '@/components/CEntryList.vue'
+import CButton from '@/components/CButton.vue'
 
 import TopBar from './TopBar.vue'
 import RouteTabGroup from './RouteTabGroup.vue'
@@ -27,15 +28,25 @@ onMounted(() => {
 <template>
   <div class="dark:bg-neutral-950 w-screen h-screen overflow-hidden flex flex-col">
     <TopBar></TopBar>
-    <div class="flex grow">
+    <div class="flex grow overflow-hidden">
       <RouteTabGroup></RouteTabGroup>
 
-      <div class="p-6 border-r dark:border-neutral-800 space-y-4">
+      <div
+        class="p-6 border-r dark:border-neutral-800 space-y-4 flex flex-col items-stretch overflow-hidden shrink-0"
+      >
         <StatisticPanel></StatisticPanel>
         <CDivider></CDivider>
-        <CEntryList></CEntryList>
+        <CEntryList class="overflow-y-auto"></CEntryList>
+        <CDivider></CDivider>
+        <CButton icon=" ph-plus" class="w-fit mx-auto"></CButton>
       </div>
       <RouterView></RouterView>
     </div>
   </div>
 </template>
+
+<style scoped>
+.c-button {
+  @apply rounded-full;
+}
+</style>
