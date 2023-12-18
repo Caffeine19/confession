5bc52431c35df47576ef5ee2479e090cf5e3597e
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2023-12-18 11:20:36 +0800

    :sparkles: CDateInput use current year as default value

--- src/components/CDateInput.vue
+++ src/components/CDateInput.vue
@@ -4,12 +4,31 @@ import dayjs from 'dayjs'
 import CInput from './CInput.vue'
 
 defineProps<{ value: any }>()
-defineEmits<{ 'update:value': [val: string] }>()
+const emits = defineEmits<{ 'update:value': [val: string] }>()
+
+const parseDateWithDefaultYear = (dateString: string) => {
+  const currentDate = dayjs()
+  const parsedDate = dayjs(dateString, 'MM-DD')
+
+  if (parsedDate.isValid()) {
+    if (parsedDate.year() === 2001) {
+      console.log(parsedDate.year(), dayjs().year())
+
+      return parsedDate.year(currentDate.year()).format('YYYY-MM-DD')
+    } else {
+      return parsedDate.format('YYYY-MM-DD')
+    }
+  } else {
+    console.error('Invalid date string')
+
+    return ''
+  }
+}
+
+const onChange = (newVal: string) => {
+  emits('update:value', parseDateWithDefaultYear(newVal))
+}
 </script>
 <template>
-  <CInput
-    :value="value"
-    icon="ph-calendar-plus"
-    @change="(newVal) => $emit('update:value', dayjs(newVal).format('DD-MM-YYYY'))"
-  ></CInput>
+  <CInput :value="value" icon="ph-calendar-plus" @change="onChange"></CInput>
 </template>
